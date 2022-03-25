8198b1163f3c6fef697a0f6ee0b6fc14f81bffc6
andreslf29 <80796729+andreslf29@users.noreply.github.com>
2022-03-25 09:44:20 -0500

    Entrega Final

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
-import React, {createContext} from "react";
+import React from "react";
 import "./styles/App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Cart from "./components/Cart";
-import Hero from "./components/Hero";
 import ItemListContainer from "./containers/ItemListContainer";
 import ItemDetailContainer from "./containers/ItemDetailContainer";
-import  {CartContextProvider}  from "./context/CartContext";
+import { CartContextProvider } from "./context/CartContext";
 import CartView from "./components/Cart/CartView";
 import Checkout from "./components/Checkout";
-
-
 
 function App() {
   return (
     <>
-      {/*<Hero />*/}
       <CartContextProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<ItemListContainer />} />
-          <Route
-            path="/categoria/:categoryid"
-            element={<ItemListContainer />}
-          />
-          <Route path="/item/:id" element={<ItemDetailContainer />} />
-          <Route path="/cart" element={<CartView />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="*" element={<>NOT FOUND</>} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<ItemListContainer />} />
+            <Route
+              path="/categoria/:categoryid"
+              element={<ItemListContainer />}
+            />
+            <Route path="/item/:id" element={<ItemDetailContainer />} />
+            <Route path="/cart" element={<CartView />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="*" element={<>NOT FOUND</>} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
       </CartContextProvider>
-      
     </>
   );
 }
